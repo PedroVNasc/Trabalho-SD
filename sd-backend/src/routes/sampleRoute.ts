7d810eaabfd3a8a907a5d3d0cdcadef3847181a9
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2024-06-25 20:05:58 -0300

    New portal

--- sd-backend/src/routes/sampleRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getSample } from '../controllers/sampleController';
-
-const router = Router();
-
-router.get('/sample', getSample);
-
-export default router;
