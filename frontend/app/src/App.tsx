105a0fb5efe55fe57985af5c39475fabfb9dce60
KrulesExGamer <57680132+PedroVNasc@users.noreply.github.com>
2024-06-26 14:09:18 -0300

    README
    Primeira versão

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -32,13 +32,13 @@ function TabPanel(props: TabPanelProps) {
   );
 }
 
-interface FormItemProps {
+interface FormTextProps {
   label: string,
   id: string,
   size?: number
 }
 
-function FormItem(props: FormItemProps) {
+function FormText(props: FormTextProps) {
   return (
     <Grid item xs={props.size ?? 6}>
       <TextField label={props.label} id={props.id} variant='standard' />
@@ -96,47 +96,47 @@ function App() {
       <TabPanel value={value} index={0}>
         <h2>Informações da Clinica</h2>
         <Grid container spacing={2}>
-          <FormItem label='ID' id='clinic_id' />
-          <FormItem label='Nome' id='clinic_name' />
+          <FormText label='ID' id='clinic_id' />
+          <FormText label='Nome' id='clinic_name' />
         </Grid>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <h2>Informações do Médico</h2>
         <Grid container spacing={2}>
-          <FormItem label='ID' id='doctor_id' size={4} />
-          <FormItem label='Nome' id='doctor_name' size={4} />
-          <FormItem label='Email' id='doctor_email' size={4} />
-          <FormItem label='Telefone' id='doctor_phone' size={4} />
-          <FormItem label='Endereço' id='doctor_adress' size={4} />
-          <FormItem label='Cidade' id='doctor_city' size={4} />
-          <FormItem label='Estado' id='doctor_state' size={4} />
-          <FormItem label='CEP' id='doctor_zip' size={4} />
-          <FormItem label='Especialidade' id='doctor_specialty' size={4} />
-          <FormItem label='CRM' id='doctor_crm' size={4} />
+          <FormText label='ID' id='doctor_id' size={4} />
+          <FormText label='Nome' id='doctor_name' size={4} />
+          <FormText label='Email' id='doctor_email' size={4} />
+          <FormText label='Telefone' id='doctor_phone' size={4} />
+          <FormText label='Endereço' id='doctor_adress' size={4} />
+          <FormText label='Cidade' id='doctor_city' size={4} />
+          <FormText label='Estado' id='doctor_state' size={4} />
+          <FormText label='CEP' id='doctor_zip' size={4} />
+          <FormText label='Especialidade' id='doctor_specialty' size={4} />
+          <FormText label='CRM' id='doctor_crm' size={4} />
         </Grid>
       </TabPanel>
       <TabPanel value={value} index={2}>
         <h2>Informações do Seguro</h2>
         <Grid container spacing={2}>
-          <FormItem label='Nome' id='insurance_name' />
-          <FormItem label='Região' id='insurance_region' />
+          <FormText label='Nome' id='insurance_name' />
+          <FormText label='Região' id='insurance_region' />
         </Grid>
       </TabPanel>
       <TabPanel value={value} index={3}>
         <h2>Informações do Medicamento</h2>
         <Grid container spacing={2}>
-          <FormItem label='Nome' id='medicine_name' />
-          <FormItem label='Nome Genérico' id='medicine_generic' />
-          <FormItem label='Marca' id='medicine_brand' />
-          <FormItem label='Dosagem' id='medicine_dosage' />
-          <FormItem label='Força' id='medicine_strength' />
-          <FormItem label='Fabricante' id='medicine_manufacturer' />
-          <FormItem label='Data de Expiração' id='medicine_expiration' />
-          <FormItem label='Número de Lote' id='medicine_batch' />
-          <FormItem label='Indicações' id='medicine_indications' />
-          <FormItem label='Preço' id='medicine_price' />
-          <FormItem label='Exige prescricão?' id='medicine_prescription' />
-          <FormItem label='Quantidade' id='medicine_quantity' />
+          <FormText label='Nome' id='medicine_name' />
+          <FormText label='Nome Genérico' id='medicine_generic' />
+          <FormText label='Marca' id='medicine_brand' />
+          <FormText label='Dosagem' id='medicine_dosage' />
+          <FormText label='Força' id='medicine_strength' />
+          <FormText label='Fabricante' id='medicine_manufacturer' />
+          <FormText label='Data de Expiração' id='medicine_expiration' />
+          <FormText label='Número de Lote' id='medicine_batch' />
+          <FormText label='Indicações' id='medicine_indications' />
+          <FormText label='Preço' id='medicine_price' />
+          <FormText label='Exige prescricão?' id='medicine_prescription' />
+          <FormText label='Quantidade' id='medicine_quantity' />
         </Grid>
       </TabPanel>
       <TabPanel value={value} index={4}>
@@ -149,7 +149,16 @@ function App() {
         PrescriptionRequest
       </TabPanel>
       <TabPanel value={value} index={7}>
-        User
+        <h2>Informações do Usuário</h2>
+        <Grid container spacing={2}>
+          <FormText label='CNS' id='user_CNS' />
+          <FormText label='Nome' id='user_name' />
+          <FormText label='Data de Nascimento' id='user_birthdate' />
+          <FormText label='Email' id='user_email' />
+          <FormText label='Telefone' id='user_phone' />
+          <FormText label='Senha' id='user_password' />
+          <FormText label='Sexo' id='user_sex' />
+        </Grid>
       </TabPanel>
     </Box>
   );
